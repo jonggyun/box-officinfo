9395ad5be004615e0802f490e398cafa89fe3ca8
JongGyun Park <jonggyuni@gmail.com>
2019-04-06 02:36:30 +0900

    link moivie detail

--- src/components/boxoffice/BoxofficeList.tsx
+++ src/components/boxoffice/BoxofficeList.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { breakpoints } from '../../lib/styles/responsive';
@@ -30,60 +31,69 @@ const MovieLi = styled.li`
   box-shadow: 0.125rem 0.1875rem #8bc2bb;
   margin-bottom: 1.875rem;
   color: #004e45;
-
-  /* 순위 */
-  & > div:nth-child(1) {
-    width: 4.375rem;
+  a {
     display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    padding-left: 0.3125rem;
-    text-align: center;
-    @media screen and (max-width: ${breakpoints.desktop}) {
-      width: 3rem;
-    }
-    div:first-child {
-      font-size: 1.5625rem;
-      font-weight: bold;
-      @media screen and (max-width: ${breakpoints.desktop}) {
-        font-size: 1.25rem;
-      }
-    }
-
-    div:last-child {
-      font-size: 0.8125rem;
-      @media screen and (max-width: ${breakpoints.desktop}) {
-        font-size: 0.625rem;
-      }
-    }
   }
-  /* 제목 */
-  & > div:nth-child(2) {
-    font-size: 1.5rem;
-    margin-top: auto;
-    margin-bottom: auto;
-    max-width: 55%;
+  &:hover {
+    box-shadow: 3px 3px #000;
+  }
+`;
+
+const MovieRank = styled.div`
+  /* 순위 */
+  width: 4.375rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding-left: 0.3125rem;
+  text-align: center;
+  @media screen and (max-width: ${breakpoints.desktop}) {
+    width: 3rem;
+  }
+  div:first-child {
+    font-size: 1.5625rem;
+    font-weight: bold;
     @media screen and (max-width: ${breakpoints.desktop}) {
-      font-size: 1.125rem;
-    }
-    @media screen and (max-width: ${breakpoints.tablet}) {
-      font-size: 1rem;
+      font-size: 1.25rem;
     }
   }
 
-  /* 정보 */
-  & > div:nth-child(3) {
-    text-align: right;
-    min-width: 7.5rem;
+  div:last-child {
     font-size: 0.8125rem;
-    margin-top: auto;
-    margin-bottom: 0.625rem;
-    margin-right: 0.625rem;
-    color: #868e96;
     @media screen and (max-width: ${breakpoints.desktop}) {
       font-size: 0.625rem;
-      min-width: 0rem;
     }
+  }
+`;
+
+const MovieTitle = styled.div`
+  /* 제목 */
+  font-size: 1.5rem;
+  margin-top: auto;
+  margin-bottom: auto;
+  /* max-width: 55%; */
+  cursor: pointer;
+  color: #004e45;
+  @media screen and (max-width: ${breakpoints.desktop}) {
+    font-size: 1.125rem;
+  }
+  @media screen and (max-width: ${breakpoints.tablet}) {
+    font-size: 1rem;
+  }
+`;
+
+const MovieInfo = styled.div`
+  /* 정보 */
+  text-align: right;
+  min-width: 7.5rem;
+  font-size: 0.8125rem;
+  margin-top: auto;
+  margin-bottom: 0.625rem;
+  margin-right: 0.625rem;
+  color: #868e96;
+  @media screen and (max-width: ${breakpoints.desktop}) {
+    font-size: 0.625rem;
+    min-width: 0rem;
   }
 `;
 interface RankProps {
@@ -113,6 +123,7 @@ const BoxofficeList: React.FC<BoxofficeListProps> = ({ type, boxoffice }) => {
         {boxoffice.map(movie => (
           <MovieComponent
             key={movie.movieCd}
+            movieCd={movie.movieCd}
             rank={movie.rank}
             rankInten={movie.rankInten}
             rankOldAndNew={movie.rankOldAndNew}
@@ -127,6 +138,7 @@ const BoxofficeList: React.FC<BoxofficeListProps> = ({ type, boxoffice }) => {
 };
 
 interface MovieComponent {
+  movieCd: string;
   rank: number;
   rankInten: number;
   rankOldAndNew: string;
@@ -136,7 +148,7 @@ interface MovieComponent {
 }
 const MovieComponent = (props: MovieComponent) => (
   <MovieLi>
-    <div>
+    <MovieRank>
       <div>{props.rank}</div>
       <div>
         {Number(props.rankInten) === 0 ? (
@@ -151,16 +163,18 @@ const MovieComponent = (props: MovieComponent) => (
           </Rank>
         )}
       </div>
-    </div>
-    <div>
-      {props.movieNm.length < 13
-        ? props.movieNm
-        : props.movieNm.slice(0, 13).concat('...')}
-    </div>
-    <div>
+    </MovieRank>
+    <Link to={`/movie/${props.movieCd}`}>
+      <MovieTitle>
+        {props.movieNm.length < 13
+          ? props.movieNm
+          : props.movieNm.slice(0, 13).concat('...')}
+      </MovieTitle>
+    </Link>
+    <MovieInfo>
       <div>{props.openDt.replace(/-/g, '.')} 개봉</div>
       <div>누적 {props.audiAcc.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}명</div>
-    </div>
+    </MovieInfo>
   </MovieLi>
 );
 export default BoxofficeList;
